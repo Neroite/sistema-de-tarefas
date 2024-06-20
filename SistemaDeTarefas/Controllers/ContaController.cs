2a90ca0a9f2e74618e44e35d3a1379115e4a4744
Nathan Rocha <neroite71@hotmail.com>
2024-06-19 21:07:35 -0300

    Criando Token de Autorização

---
+++ SistemaDeTarefas/Controllers/ContaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SistemaDeTarefas.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SistemaDeTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            if(login.Login == "admin" && login.Senha == "admin")
+            {
+                var token = GerarTokenJWT();
+                return Ok(new { token });
+            }
+
+            return BadRequest(new {mensagem = "Credencias inválidas. Por favor, verifique seu nome de usuário e senha"});
+        }
+
+        private string GerarTokenJWT()
+        {
+            string chaveSecreta = "05027c82-8ef7-4045-a9af-4f2b6d54f739";
+
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveSecreta));
+            var credencial = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("login", "admin"),
+                new Claim("nome", "Administrador do Sistema")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "sua_empresa",
+                audience: "sua_aplicacao",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credencial
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ d
@@ -0,0 +1,41 @@
+[33mcommit 77898b802cf8d9f74d95e8238d01f9c51d9393a7[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Nathan Rocha <neroite71@hotmail.com>
+Date:   Wed Jun 19 20:24:25 2024 -0300
+
+    Criando Proteção da API
+
+[33mcommit 6b568f29a7af0d14ff59224d8626e23d75444746[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: Nathan Rocha <neroite71@hotmail.com>
+Date:   Tue Jun 18 21:51:32 2024 -0300
+
+    Consumindo API que busca o endereco pelo CEP
+
+[33mcommit 7e71578823b42861ac0eef5046fda4d89c731c54[m
+Author: Nathan Rocha <neroite71@hotmail.com>
+Date:   Mon Jun 17 21:42:58 2024 -0300
+
+    CRUD Tarefas
+
+[33mcommit bcad8b5055cf4026094f4a2553fc996a827ab327[m
+Author: Nathan Rocha <neroite71@hotmail.com>
+Date:   Thu Jun 13 22:21:17 2024 -0300
+
+    Vinculando de Usuario e Tarefa
+
+[33mcommit cf3eaf34319984449f926cf55ce80aceddb07ac2[m
+Author: Nathan Rocha <neroite71@hotmail.com>
+Date:   Wed Jun 12 21:37:20 2024 -0300
+
+    Configurando Banco de Dados e Fazendo o CRUD
+
+[33mcommit 6037ff798561bc495f3ec89bbf2b5577fe50a44d[m
+Author: Nathan Rocha <neroite71@hotmail.com>
+Date:   Mon Mar 11 13:49:48 2024 -0300
+
+    Projeto criado
+
+[33mcommit f74bf73b85309f9dda30184f2910ad0ce3b5df8b[m
+Author: Nathan Rocha <neroite71@hotmail.com>
+Date:   Mon Mar 11 13:45:22 2024 -0300
+
+    Initial commit
